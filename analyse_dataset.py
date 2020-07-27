851b6f2adc1b74d6522e22b7f426f8a69791ed08
bscout9956 <bs.blackscout.bs@gmail.com>
2020-07-26 21:58:12 -0300

    Implement funny fake clustering code
    bruh

---
+++ analyse_dataset.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d  # unused?
+import os
+from os import sep
+from PIL import Image as Im
+from extras import extrasUtil
+
+fig = plt.figure()
+axis = plt.axes(projection='3d')
+
+input_folder = ".{0}input{0}".format(sep)
+
+red_values = list()
+green_values = list()
+blue_values = list()
+
+
+def avg_color(color_values):
+    agg_val = 0
+    for val in color_values:
+        agg_val += val
+    return agg_val / len(color_values)
+
+
+def main():
+    idx_limit = extrasUtil.check_file_count(input_folder)
+    idx = 1
+    for file in os.listdir(input_folder):
+        print("Processing file {} | {} of {} | {:.2f} percent".format(file, idx, idx_limit, (idx / idx_limit) * 100))
+        temp_red = list()
+        temp_green = list()
+        temp_blue = list()
+
+        file_path = input_folder + file
+        with Im.open(file_path, "r") as image:
+            image_data = image.load()
+
+            for x in range(image.width):
+                for y in range(image.height):
+                    temp_red.append(image_data[x, y][0])
+                    temp_green.append(image_data[x, y][1])
+                    temp_blue.append(image_data[x, y][2])
+
+            avg_colors = round(avg_color(temp_red)), round(avg_color(temp_green)), round(avg_color(temp_blue))
+            red_values.append(avg_colors[0])
+            green_values.append(avg_colors[1])
+            blue_values.append(avg_colors[2])
+        idx += 1
+
+    # axis.plot3D(red_values, green_values, blue_values)
+    axis.scatter(red_values, green_values, blue_values, color="black")
+    axis.set_xlabel("Red")
+    axis.set_ylabel("Green")
+    axis.set_zlabel("Blue")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
