8b4f0215cf0109a8fa085d3f6ff54ea586711699
bscout9956 <bs.blackscout.bs@gmail.com>
2020-07-29 21:46:00 -0300

    Rename files

---
+++ extras/color_stuff_prepare.py
@@ -0,0 +1,52 @@
+import os
+
+from PIL import Image as Im
+from PIL import ImageOps as ImOps
+
+import extrasUtil
+
+# Helper variables
+
+slash = "\\" if os.name == 'nt' else "/"
+valid_extensions = (".jpg", ".png", ".dds", ".bmp")
+
+
+def process(input_folder):
+    file_count = extrasUtil.check_file_count(input_folder)
+    index = 1
+    failed_files = 0
+    skipped_files = 0
+    for root, dirs, files in os.walk(input_folder):
+        for filename in files:     
+            if filename.endswith(valid_extensions):
+                print("Processing Picture {} of {}".format(index, file_count))
+                pic_path = "{0}{1}{2}".format(root, slash, filename)
+                try:
+                    picture = Im.open(pic_path, "r")
+                    #picture = ImOps.posterize(picture, 8) # 8 bit
+                    picture = picture.convert(mode="P", dither=None, palette=Im.ADAPTIVE) # Should I even convert??
+                    picture = picture.convert(mode="RGB")
+                    for ext in valid_extensions:
+                        pic_path.replace(ext, ".png")
+                    picture.save(pic_path, "PNG", icc_profile='')
+                    index += 1
+                except Exception as e:
+                    raise e
+                    print("An error prevented this image from being converted")
+                    print("Delete: {} ?".format(pic_path))
+                    failed_files += 1
+            else:
+                print("Skipped {} as it's not a valid image or not a valid extension.".format(filename))
+                skipped_files += 1
+
+    print("{} pictures failed to be processed.".format(failed_files))
+    print("{} files were skipped.".format(skipped_files))
+
+
+def main():
+    process("..{0}datasets{0}train{0}lr".format(slash))
+    process("..{0}datasets{0}val{0}lr".format(slash))
+
+
+if __name__ == "__main__":
+    main()
